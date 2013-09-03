061229dee6340daf8c043b64c636855eb8742e4d
jon uy <jonxuy@gmail.com>
2013-09-03 01:38:25 -0400

    Back button added to About page

--- src/com/jonuy/uy_app/About.java
+++ src/com/jonuy/uy_app/About.java
@@ -1,13 +1,25 @@
 package com.jonuy.uy_app;
 
-import android.app.Activity;
+import roboguice.activity.RoboActivity;
+import roboguice.inject.InjectView;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class About extends Activity {
+public class About extends RoboActivity {
 
+	@InjectView(R.id.button_back) private Button mBackButton;
+	
 	@Override
 	protected void onCreate(Bundle savedInstance) {
 		super.onCreate(savedInstance);
 		setContentView(R.layout.activity_about);
+		
+		mBackButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				finish();
+			}
+		});
 	}
 }

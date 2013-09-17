7390bf3d78b34067e1920311b5c1b93978d4510c
Jon Uy <jonxuy@gmail.com>
2013-09-17 17:57:57 -0400

    Basic code for Projects page ViewPager slider

---
+++ src/com/jonuy/uy_app/ProjectFragment.java
@@ -0,0 +1,17 @@
+package com.jonuy.uy_app;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ProjectFragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_project_slide, null);
+		
+		return rootView;
+	}
+}
--- src/com/jonuy/uy_app/Projects.java
+++ src/com/jonuy/uy_app/Projects.java
@@ -1,13 +1,42 @@
 package com.jonuy.uy_app;
 
-import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
+import android.support.v13.app.FragmentStatePagerAdapter;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 
-public class Projects extends Activity {
+public class Projects extends FragmentActivity {
+	
+	private static final int NUM_PAGES = 3;
+	private ViewPager mPager;
+	private PagerAdapter mPagerAdapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstance) {
 		super.onCreate(savedInstance);
 		setContentView(R.layout.activity_projects);
+		
+		mPager = (ViewPager)findViewById(R.id.pager);
+		mPagerAdapter = new ScreenSlidePagerAdapter(getFragmentManager());
+		mPager.setAdapter(mPagerAdapter);
+	}
+	
+	private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+		public ScreenSlidePagerAdapter(FragmentManager fm) {
+			super(fm);
+		}
+
+		@Override
+		public Fragment getItem(int position) {
+			return new ProjectFragment();
+		}
+
+		@Override
+		public int getCount() {
+			return NUM_PAGES;
+		}
 	}
 }
